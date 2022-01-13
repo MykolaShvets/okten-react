6bf2b0f83ccc06f15e476d2c1bb8f4bc85984c66
MykolaShvets <m2009sv@gmail.com>
2022-01-13 17:13:54 +0200

    HomeWork2 repair

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+
+import './App.css'
 import Users from "./components/Users/Users";
+import {userServices} from "./services/user.services";
+import {postServices} from "./services/post.services";
+import React, {useState} from "react";
+import UserDetails from "./components/UserDetails/UserDetails";
+import Post from "./components/Post/Post";
+
+
 
 function App() {
-  return (
-    <div>
-      <Users/>
-    </div>
-  );
+
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+
+    const getUserId = (id) => {
+        userServices.getById(id)
+            .then(value => setUser(value))
+    };
+
+    const getUserPosts = (id) => {
+        postServices.getPostsByUserId(id)
+            .then(value => setPosts(value))
+    };
+
+    return (
+        <div className={'wrapp'}>
+            <div className={'top'}>
+                <div className={'users'}>
+                    <Users getUserId={getUserId} getUserPost={getUserPosts}/>
+                </div>
+                <div className={'details'}>
+                    {user && <UserDetails key={user.id} user={user} getUserPosts={getUserPosts}/>}
+                </div>
+            </div>
+            <div className={'posts'}>
+                {posts && posts.map(post => <Post key={post.id} post={post}/>)}
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,49 +1,25 @@
 import React, {useEffect, useState} from 'react';
 
-import './Users.css'
 import User from "../User/User";
-import UserDetails from "../UserDetails/UserDetails";
-import Post from "../Post/Post";
 import {userServices} from "../../services/user.services";
-import {postServices} from "../../services/post.services";
 
-const Users = () => {
+
+
+
+const Users = ({getUserId}) => {
 
     const [users, setUsers] = useState([]);
-    const [user, setUser] = useState(null);
-    const [posts, setPosts] = useState([]);
-
 
     useEffect(() => {
             userServices.getAll()
             .then(value => setUsers(value))
     }, []);
 
-    const getUserId = (id) => {
-        userServices.getById(id)
-            .then(value => setUser(value))
-    };
 
-    const getUserPosts = (id) => {
-        postServices.getPostsByUserId(id)
-            .then(value => setPosts(value))
-    };
 
     return (
         <div>
-            <div className={'top'}>
-                <div className={'users'}>
                     {users.map(user => <User key={user.id} user={user} getUserId={getUserId}/>)}
-                </div>
-                <div className={'details'}>
-                    {user && <UserDetails key={user.id} user={user} getUserPosts={getUserPosts}/>}
-                </div>
-            </div>
-
-            <div className={'posts'}>
-                {posts && posts.map(post => <Post key={post.id} post={post}/>)}
-            </div>
-
         </div>
     );
 };
