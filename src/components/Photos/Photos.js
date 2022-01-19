653bf9c4b72733d9a2dd1c88e41b54be55e7c8fa
MykolaShvets <m2009sv@gmail.com>
2022-01-19 11:04:47 +0200

    ClassWork3

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+
 import {albumService} from "../../services/album.service";
 import Photo from "../Photo/Photo";
 
